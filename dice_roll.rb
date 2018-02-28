48f2bb2b8c3eacdc3ad5c06f6cb882a50a566ccf
William Dale <williambdale@gmail.com>
2018-02-28 20:13:16 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,7 +3,7 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  choices = (1..6).to_a 
-  idx = Random.rand(0..5)
-  choices[idx]
+  # choices = (1..6).to_a 
+  # idx = Random.rand(0..5)
+  # choices[idx]
 end
